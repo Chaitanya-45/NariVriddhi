d03d64040cc2c198fb3d885eb0cd50c821590096
Chaitanya <139270341+Chaitanya-45@users.noreply.github.com>
2024-03-30 15:08:58 +0530

    Update login.php

--- login.php
+++ login.php
@@ -30,7 +30,7 @@ mysqli_close($conn);
 <head>
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inspo - Register</title>
+    <title>Login</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="profstyle.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
