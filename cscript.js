02c22d49ae9a26ab36ef48a5b5ba88f28288b4e2
manvitha984 <146608418+manvitha984@users.noreply.github.com>
2024-03-31 19:51:27 +0530

    chore

--- cscript.js
+++ cscript.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null;
-const API_KEY = ""; 
+const API_KEY = ""; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
